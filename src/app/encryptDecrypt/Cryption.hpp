346d55eeb14da3c6e618389b7b72cef29779467b
vedantbhardwaj15 <bhardwajved15@gmail.com>
2026-07-29 00:51:39 +0530

    update encryption signatures to accept pre-derived key

--- src/app/encryptDecrypt/Cryption.hpp
+++ src/app/encryptDecrypt/Cryption.hpp
@@ -3,11 +3,14 @@
 
 #include <filesystem>
 #include <string>
+#include <sodium.h>
 
 bool initializeCrypto();
 
-bool encryptFile(const std::filesystem::path &inputPath, const std::filesystem::path &outputPath, const std::string &password);
+bool deriveKeyFromPassword(const std::string &password, unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]);
 
-bool decryptFile(const std::filesystem::path &inputPath, const std::filesystem::path &outputPath, const std::string &password);
+bool encryptFile(const std::filesystem::path &inputPath, const std::filesystem::path &outputPath, const unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]);
+
+bool decryptFile(const std::filesystem::path &inputPath, const std::filesystem::path &outputPath, const unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]);
 
 #endif
